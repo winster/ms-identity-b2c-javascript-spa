8520ec5b1878758d8bc0fc47e2b26652ec1535b7
derisen <dogan.erisen@gmail.com>
2020-11-26 15:38:33 -0800

    sync with main

--- api.js
+++ api.js
@@ -1,27 +1,27 @@
 function callApi(endpoint, token) {
     
-    const headers = new Headers();
-    const bearer = `Bearer ${token}`;
-  
-    headers.append("Authorization", bearer);
-  
-    const options = {
-        method: "GET",
-        headers: headers
-      };
-  
-    logMessage('Calling Web API...');
-    
-    fetch(endpoint, options)
-      .then(response => response.json())
-      .then(response => {
+  const headers = new Headers();
+  const bearer = `Bearer ${token}`;
 
-        if (response) {
-          logMessage('Web API responded: ' + response.name);
-        }
-        
-        return response;
-      }).catch(error => {
-        console.error(error);
-      });
-  }
+  headers.append("Authorization", bearer);
+
+  const options = {
+      method: "GET",
+      headers: headers
+    };
+
+  logMessage('Calling Web API...');
+  
+  fetch(endpoint, options)
+    .then(response => response.json())
+    .then(response => {
+
+      if (response) {
+        logMessage('Web API responded: ' + response.name);
+      }
+      
+      return response;
+    }).catch(error => {
+      console.error(error);
+    });
+}

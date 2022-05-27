d263208ebf3a1794267298b63f6e95261b6ac388
winster <wtjose@gmail.com>
2022-05-27 18:49:19 +0530

    Update authConfig.js

--- App/authConfig.js
+++ App/authConfig.js
@@ -8,10 +8,10 @@
 
 const msalConfig = {
     auth: {
-      clientId: "2fdd06f3-7b34-49a3-a78b-0cf1dd87878e", // This is the ONLY mandatory field; everything else is optional.
+      clientId: "9904bdb1-ebba-4ea1-8dd7-d495c52f9802", // This is the ONLY mandatory field; everything else is optional.
       authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
       knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
-      redirectUri: "http://localhost:6420", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
+      redirectUri: "https://contoso-app1.azurewebsites.net/callback", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
     },
     cache: {
       cacheLocation: "sessionStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.

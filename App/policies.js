72986df7c68085a19c6d75b72bfbc5b99ebe8680
winster <wtjose@gmail.com>
2022-05-27 19:02:17 +0530

    Update policies.js

--- App/policies.js
+++ App/policies.js
@@ -5,16 +5,12 @@
  */
 const b2cPolicies = {
     names: {
-        signUpSignIn: "B2C_1_susi_reset_v2",
-        editProfile: "B2C_1_edit_profile_v2"
+        signUpSignIn: "B2C_1_susi"
     },
     authorities: {
         signUpSignIn: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_susi_reset_v2",
-        },
-        editProfile: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_edit_profile_v2"
+            authority: "https://winstertestorg101.onmicrosoft.com.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_susi_reset_v2",
         }
     },
-    authorityDomain: "fabrikamb2c.b2clogin.com"
+    authorityDomain: "winstertestorg101.onmicrosoft.com.b2clogin.com"
 }
